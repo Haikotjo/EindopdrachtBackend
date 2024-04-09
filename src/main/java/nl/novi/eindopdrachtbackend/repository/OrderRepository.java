531b32518fbe9f05da9c74315253149c61a44471
Haikotjo <haikowierda@hotmail.com>
2024-04-09 13:48:23 +0200

    Completed Restaurant repository with tests

--- src/main/java/nl/novi/eindopdrachtbackend/repository/OrderRepository.java
+++ src/main/java/nl/novi/eindopdrachtbackend/repository/OrderRepository.java
@@ -12,4 +12,5 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("SELECT o FROM Order o WHERE o.restaurant.id = :restaurantId")
     List<Order> findOrdersByRestaurantId(@Param("restaurantId") Long restaurantId);
+
 }
--- src/main/java/nl/novi/eindopdrachtbackend/repository/RestaurantRepository.java
+++ src/main/java/nl/novi/eindopdrachtbackend/repository/RestaurantRepository.java
@@ -3,5 +3,8 @@ package nl.novi.eindopdrachtbackend.repository;
 import nl.novi.eindopdrachtbackend.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    List<Restaurant> findByNameIgnoreCase(String name);
 }
