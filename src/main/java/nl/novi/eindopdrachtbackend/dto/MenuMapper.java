52e057c7d7ab45c2909071affd0cb46fc8f936a1
Haikotjo <haikowierda@hotmail.com>
2024-04-20 14:21:00 +0200

    MenuMapper done

--- src/main/java/nl/novi/eindopdrachtbackend/dto/MenuMapper.java
+++ src/main/java/nl/novi/eindopdrachtbackend/dto/MenuMapper.java
@@ -1,4 +1,30 @@
 package nl.novi.eindopdrachtbackend.dto;
 
+import nl.novi.eindopdrachtbackend.model.Menu;
+import nl.novi.eindopdrachtbackend.model.MenuItem;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MenuMapper {
+
+    public static MenuDTO toMenuDTO(Menu menu) {
+        MenuDTO dto = new MenuDTO();
+        dto.setId(menu.getId());
+        dto.setName(menu.getName());
+        dto.setDescription(menu.getDescription());
+        if (menu.getMenuItems() != null) {
+            List<MenuItemDTO> menuItemDTOS = menu.getMenuItems().stream()
+                    .map(MenuItemMapper::toMenuItemDTO)
+                    .collect(Collectors.toList());
+            dto.setMenuItems(menuItemDTOS);
+        }
+        return dto;
+    }
+
+    public static Menu toMenu(MenuInputDTO inputDTO) {
+        Menu menu = new Menu();
+        menu.setName(inputDTO.getName());
+        menu.setDescription(inputDTO.getDescription());
+        return menu;
+    }
 }
