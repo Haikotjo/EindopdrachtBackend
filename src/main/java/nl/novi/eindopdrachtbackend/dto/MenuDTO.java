c21c577cedf1c0d3425eb4b4a0b09eef1795e250
Haikotjo <haikowierda@hotmail.com>
2024-04-20 14:14:29 +0200

    MenuDTO done

--- src/main/java/nl/novi/eindopdrachtbackend/dto/MenuDTO.java
+++ src/main/java/nl/novi/eindopdrachtbackend/dto/MenuDTO.java
@@ -1,4 +1,56 @@
 package nl.novi.eindopdrachtbackend.dto;
 
+import java.util.List;
+
 public class MenuDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private List<MenuItemDTO> menuItems;
+
+    // Constructor
+    public MenuDTO() {
+    }
+
+    public MenuDTO(Long id, String name, String description, List<MenuItemDTO> menuItems) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.menuItems = menuItems;
+    }
+
+    // Getters en setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<MenuItemDTO> getMenuItems() {
+        return menuItems;
+    }
+
+    public void setMenuItems(List<MenuItemDTO> menuItems) {
+        this.menuItems = menuItems;
+    }
 }
