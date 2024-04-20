1b5d056d565b5bd1e186a6b07089c0bbd8235904
Haikotjo <haikowierda@hotmail.com>
2024-04-20 14:17:41 +0200

    MenuInoutDTO done

--- src/main/java/nl/novi/eindopdrachtbackend/dto/MenuInputDTO.java
+++ src/main/java/nl/novi/eindopdrachtbackend/dto/MenuInputDTO.java
@@ -3,5 +3,33 @@ package nl.novi.eindopdrachtbackend.dto;
 import java.util.List;
 
 public class MenuInputDTO {
+    private String name;
+    private String description;
+    private List<Long> menuItemsIds;
 
+    // Getters en Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Long> getMenuItemsIds() {
+        return menuItemsIds;
+    }
+
+    public void setMenuItemsIds(List<Long> menuItemsIds) {
+        this.menuItemsIds = menuItemsIds;
+    }
 }
