0e1525a57972f5feefb048ccfdeb6cf45fe8e6c9
Haikotjo <haikowierda@hotmail.com>
2024-04-21 18:38:26 +0200

    UserController done. Can't get the al test to work but I'm giving up

--- src/main/java/nl/novi/eindopdrachtbackend/controller/UserController.java
+++ src/main/java/nl/novi/eindopdrachtbackend/controller/UserController.java
@@ -1,7 +1,10 @@
 package nl.novi.eindopdrachtbackend.controller;
 
+import jakarta.transaction.Transactional;
+import nl.novi.eindopdrachtbackend.dto.DeliveryAddressInputDTO;
 import nl.novi.eindopdrachtbackend.dto.UserDTO;
 import nl.novi.eindopdrachtbackend.dto.UserInputDTO;
+import nl.novi.eindopdrachtbackend.model.UserRole;
 import nl.novi.eindopdrachtbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,8 +17,11 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
     private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     // Haal alle gebruikers op
     @GetMapping
@@ -45,10 +51,32 @@ public class UserController {
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 
+    // Update gebruiker en zijn/haar adres
+    @Transactional
+    @PutMapping("/{id}/address")
+    public ResponseEntity<UserDTO> updateUserAndAddress(@PathVariable Long id, @RequestBody UserInputDTO userInputDTO, @RequestBody DeliveryAddressInputDTO addressInputDTO) {
+        UserDTO updatedUser = userService.updateUserAndAddress(id, userInputDTO, addressInputDTO);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
     // Verwijder een gebruiker
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
+    }
+
+    // Zoek gebruikers op naam, ongeacht hoofdletters of kleine letters
+    @GetMapping("/search")
+    public ResponseEntity<List<UserDTO>> findByNameIgnoreCase(@RequestParam String name) {
+        List<UserDTO> users = userService.findByNameIgnoreCase(name);
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    // Zoek gebruikers op rol
+    @GetMapping("/role/{role}")
+    public ResponseEntity<List<UserDTO>> findByRole(@PathVariable UserRole role) {
+        List<UserDTO> users = userService.findByRole(role);
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
