2f873db1635912fb5bc86d75691c447e6d43e1cc
Haikotjo <haikowierda@hotmail.com>
2024-05-31 00:58:03 +0200

    OrderMapper + Test done

--- src/main/java/nl/novi/eindopdrachtbackend/dto/OrderMapper.java
+++ src/main/java/nl/novi/eindopdrachtbackend/dto/OrderMapper.java
@@ -1,4 +1,28 @@
 package nl.novi.eindopdrachtbackend.dto;
 
+import nl.novi.eindopdrachtbackend.model.Order;
+import nl.novi.eindopdrachtbackend.model.User;
+import nl.novi.eindopdrachtbackend.model.Restaurant;
+import nl.novi.eindopdrachtbackend.model.DeliveryAddress;
+
 public class OrderMapper {
+
+    public static OrderDTO toDTO(Order order) {
+        OrderDTO dto = new OrderDTO();
+        dto.setId(order.getId());
+        dto.setFulfilled(order.isFulfilled());
+        dto.setCustomerId(order.getCustomer().getId());
+        dto.setRestaurantId(order.getRestaurant().getId());
+        dto.setDeliveryAddressId(order.getDeliveryAddress().getId());
+        return dto;
+    }
+
+    public static Order fromInputDTO(OrderInputDTO inputDTO, User customer, Restaurant restaurant, DeliveryAddress deliveryAddress) {
+        Order order = new Order();
+        order.setFulfilled(inputDTO.isFulfilled());
+        order.setCustomer(customer);
+        order.setRestaurant(restaurant);
+        order.setDeliveryAddress(deliveryAddress);
+        return order;
+    }
 }
--- src/test/java/nl/novi/eindopdrachtbackend/dto/OrderMapperTest.java
+++ src/test/java/nl/novi/eindopdrachtbackend/dto/OrderMapperTest.java
@@ -1,4 +1,70 @@
 package nl.novi.eindopdrachtbackend.dto;
 
+import nl.novi.eindopdrachtbackend.model.Order;
+import nl.novi.eindopdrachtbackend.model.User;
+import nl.novi.eindopdrachtbackend.model.Restaurant;
+import nl.novi.eindopdrachtbackend.model.DeliveryAddress;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class OrderMapperTest {
+
+    @Test
+    public void toDTOTest() throws NoSuchFieldException, IllegalAccessException {
+        // Arrange
+        User customer = new User();
+        setField(customer, "id", 1L);
+
+        Restaurant restaurant = new Restaurant();
+        setField(restaurant, "id", 2L);
+
+        DeliveryAddress deliveryAddress = new DeliveryAddress();
+        setField(deliveryAddress, "id", 3L);
+
+        Order order = new Order(customer, restaurant, deliveryAddress, true);
+        setField(order, "id", 1L);
+
+        // Act
+        OrderDTO orderDTO = OrderMapper.toDTO(order);
+
+        // Assert
+        assertEquals(1L, orderDTO.getId());
+        assertTrue(orderDTO.isFulfilled());
+        assertEquals(1L, orderDTO.getCustomerId());
+        assertEquals(2L, orderDTO.getRestaurantId());
+        assertEquals(3L, orderDTO.getDeliveryAddressId());
+    }
+
+    @Test
+    public void fromInputDTOTest() throws NoSuchFieldException, IllegalAccessException {
+        // Arrange
+        OrderInputDTO orderInputDTO = new OrderInputDTO(true, 1L, 2L, 3L);
+
+        User customer = new User();
+        setField(customer, "id", 1L);
+
+        Restaurant restaurant = new Restaurant();
+        setField(restaurant, "id", 2L);
+
+        DeliveryAddress deliveryAddress = new DeliveryAddress();
+        setField(deliveryAddress, "id", 3L);
+
+        // Act
+        Order order = OrderMapper.fromInputDTO(orderInputDTO, customer, restaurant, deliveryAddress);
+
+        // Assert
+        assertTrue(order.isFulfilled());
+        assertEquals(customer, order.getCustomer());
+        assertEquals(restaurant, order.getRestaurant());
+        assertEquals(deliveryAddress, order.getDeliveryAddress());
+    }
+
+    private void setField(Object targetObject, String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
+        Field field = targetObject.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(targetObject, value);
+    }
 }
