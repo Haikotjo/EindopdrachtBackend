e282376446fecec86ade6474f75c2e619bf4ff0a
Haikotjo <haikowierda@hotmail.com>
2024-07-25 16:22:00 +0200

    Redid aoutomated notifications added some javadocs

--- src/main/java/nl/novi/eindopdrachtbackend/service/ExpirationDateCheckerService.java
+++ src/main/java/nl/novi/eindopdrachtbackend/service/ExpirationDateCheckerService.java
@@ -5,6 +5,7 @@ import nl.novi.eindopdrachtbackend.model.Ingredient;
 import nl.novi.eindopdrachtbackend.model.MenuItem;
 import nl.novi.eindopdrachtbackend.model.User;
 import nl.novi.eindopdrachtbackend.repository.IngredientRepository;
+import nl.novi.eindopdrachtbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -15,13 +16,16 @@ import java.util.List;
 @Service
 public class ExpirationDateCheckerService {
 
+
     private final IngredientRepository ingredientRepository;
+    private final UserRepository userRepository;
     private final NotificationService notificationService;
     private static final int EXPIRATION_WARNING_DAYS = 7;
 
     @Autowired
-    public ExpirationDateCheckerService(IngredientRepository ingredientRepository, NotificationService notificationService) {
+    public ExpirationDateCheckerService(IngredientRepository ingredientRepository, UserRepository userRepository, NotificationService notificationService) {
         this.ingredientRepository = ingredientRepository;
+        this.userRepository = userRepository;
         this.notificationService = notificationService;
     }
 
@@ -29,17 +33,27 @@ public class ExpirationDateCheckerService {
      * Schedules a daily check for expiring ingredients and sends notifications to owners if ingredients are expiring soon.
      * This method is executed at midnight every day.
      */
+
     @Scheduled(cron = "0 0 0 * * ?")
     public void checkExpirationDates() {
+        System.out.println("checkExpirationDates method called");
         List<Ingredient> ingredients = ingredientRepository.findAll();
         for (Ingredient ingredient : ingredients) {
+            System.out.println("Checking ingredient: " + ingredient.getName() + " with expiration date: " + ingredient.getExpirationDate());
             if (ingredient.getQuantity() > 0 && isExpiringSoon(ingredient)) {
-                for (MenuItem menuItem : ingredient.getMenuItems()) {
-                    User owner = menuItem.getRestaurant().getOwner();
+                System.out.println("Ingredient " + ingredient.getName() + " is expiring soon");
+                User owner = ingredient.getOwner();
+                if (owner != null) {
+                    System.out.println("Found owner: " + owner.getEmail() + " for ingredient: " + ingredient.getName());
                     NotificationInputDTO notificationInputDTO = new NotificationInputDTO();
                     notificationInputDTO.setMessage("Ingredient " + ingredient.getName() + " is expiring soon.");
                     notificationService.createNotification(notificationInputDTO, owner);
+                    System.out.println("Notification created for user: " + owner.getEmail() + " about ingredient: " + ingredient.getName());
+                } else {
+                    System.out.println("No owner found for ingredient: " + ingredient.getName());
                 }
+            } else {
+                System.out.println("Ingredient " + ingredient.getName() + " is not expiring soon or quantity is 0.");
             }
         }
     }
@@ -52,7 +66,9 @@ public class ExpirationDateCheckerService {
      */
     private boolean isExpiringSoon(Ingredient ingredient) {
         LocalDate expirationDate = ingredient.getExpirationDate();
-        return expirationDate.minusDays(EXPIRATION_WARNING_DAYS).isBefore(LocalDate.now());
+        LocalDate warningDate = expirationDate.minusDays(EXPIRATION_WARNING_DAYS);
+        boolean expiringSoon = warningDate.isBefore(LocalDate.now());
+        System.out.println("Ingredient " + ingredient.getName() + " expiring soon: " + expiringSoon);
+        return expiringSoon;
     }
-
 }
