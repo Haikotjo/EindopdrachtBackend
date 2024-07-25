944040293cafd16a2acdc9193033bf2d9a05a396
Haikotjo <haikowierda@hotmail.com>
2024-07-25 12:49:12 +0200

    Small changes mostly added some javadocs

--- src/main/java/nl/novi/eindopdrachtbackend/dto/AuthDTO.java
+++ src/main/java/nl/novi/eindopdrachtbackend/dto/AuthDTO.java
@@ -2,10 +2,23 @@ package nl.novi.eindopdrachtbackend.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
+/**
+ * AuthDTO is a Data Transfer Object for authentication requests.
+ * It contains the necessary information for user authentication.
+ */
 public class AuthDTO {
+
+    /**
+     * The email of the user.
+     * This field is mandatory and cannot be blank.
+     */
     @NotBlank(message = "Email is mandatory")
     public String email;
 
+    /**
+     * The password of the user.
+     * This field is mandatory and cannot be blank.
+     */
     @NotBlank(message = "Password is mandatory")
     public String password;
 }
--- src/main/java/nl/novi/eindopdrachtbackend/dto/RoleDTO.java
+++ src/main/java/nl/novi/eindopdrachtbackend/dto/RoleDTO.java
@@ -2,24 +2,49 @@ package nl.novi.eindopdrachtbackend.dto;
 
 import nl.novi.eindopdrachtbackend.model.UserRole;
 
+/**
+ * RoleDTO is a Data Transfer Object for role-related information.
+ * It contains the necessary information for role management.
+ */
 public class RoleDTO {
+
+    /**
+     * The name of the role.
+     */
     public String rolename;
 
-    // Constructor
+    /**
+     * Constructs a new RoleDTO with the specified role name.
+     *
+     * @param rolename the name of the role
+     */
     public RoleDTO(String rolename) {
         this.rolename = rolename;
     }
 
-    // Getter and Setter
+    /**
+     * Gets the name of the role.
+     *
+     * @return the role name
+     */
     public String getRolename() {
         return rolename;
     }
 
+    /**
+     * Sets the name of the role.
+     *
+     * @param rolename the name of the role to set
+     */
     public void setRolename(String rolename) {
         this.rolename = rolename;
     }
 
-    // Method to convert to UserRole enum
+    /**
+     * Converts this RoleDTO to a UserRole enum.
+     *
+     * @return the UserRole corresponding to this RoleDTO
+     */
     public UserRole toUserRole() {
         return UserRole.valueOf(this.rolename);
     }
